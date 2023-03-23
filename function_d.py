58751b3f9f25117793102a95088a9a25e5783047
Sage Choi <coolcuds@gmail.com>
2023-03-23 13:59:34 -0700

    Wrote max_value() function logic

--- function_d.py
+++ function_d.py
@@ -2,7 +2,15 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    if not numbers:
+        return None
+    
+    max_num = None
+    for num in numbers:
+        if max_num is None or max_num < num:
+            max_num = num
+    
+    return max_num
 
 
 if __name__ == "__main__":
